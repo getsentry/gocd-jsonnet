15bfc3c6755d65bef30c1af04efa65af163e8405
Matt Gaunt-Seo <matt.gaunt@sentry.io>
2023-07-07 13:10:42 -0700

    Add initial version of rollback pipeline to pipedream

--- v1.0.0/bash/rollback.sh
+++ v1.0.0/bash/rollback.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "Hello World"
+# Pause all pipelines in the pipedream
+gocd-pause-and-cancel-pipelines \
+  --pause-message="This pipeline is being rolled back, please check with team before un-pausing." \
+  <PIPELINE_FLAGS>
+
+# Get sha from the given pipeline run to deploy to all pipedream pipelines.
+sha=$(gocd-sha-for-pipeline --material-name="${ROLLBACK_MATERIAL_NAME}")
+
+gocd-emergency-deploy \
+  --commit-sha="${sha}" \
+  --deploy-stage="${ROLLBACK_STAGE}" \
+  --pause-message="This pipeline was rolled back, please check with team before un-pausing." \
+  <PIPELINE_FLAGS>
