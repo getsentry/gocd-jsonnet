d04037f4f67e94511cce84c2763b2317193537f9
Matt Gaunt-Seo <matt.gaunt@sentry.io>
2023-07-10 10:51:32 -0700

    Switch to bash friendly args

--- libs/bash/rollback.sh
+++ libs/bash/rollback.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+if [[ "${PIPELINE_FLAGS:-}" ]]; then
+  # shellcheck disable=SC2086
+  set -- $PIPELINE_FLAGS   # note: no quoting, for word expansion
+fi
+
+
 # Pause all pipelines in the pipedream
 gocd-pause-and-cancel-pipelines \
   --pause-message="This pipeline is being rolled back, please check with team before un-pausing." \
-  <PIPELINE_FLAGS>
+  "$@"
 
 # Get sha from the given pipeline run to deploy to all pipedream pipelines.
 sha=$(gocd-sha-for-pipeline --material-name="${ROLLBACK_MATERIAL_NAME}")
@@ -12,4 +18,4 @@ gocd-emergency-deploy \
   --commit-sha="${sha}" \
   --deploy-stage="${ROLLBACK_STAGE}" \
   --pause-message="This pipeline was rolled back, please check with team before un-pausing." \
-  <PIPELINE_FLAGS>
+  "$@"
