16c409ad4a1daa01f04f4e7faa5c752844088c38
Matt Gaunt-Seo <matt.gaunt@sentry.io>
2023-07-18 17:03:36 -0700

    Add debug message

--- libs/bash/rollback.sh
+++ libs/bash/rollback.sh
@@ -9,6 +9,8 @@ fi
 # Get sha from the given pipeline run to deploy to all pipedream pipelines.
 sha=$(gocd-sha-for-pipeline --material-name="${ROLLBACK_MATERIAL_NAME}")
 
+echo "📑 Rolling back to sha: ${sha}"
+
 gocd-emergency-deploy \
   --commit-sha="${sha}" \
   --deploy-stage="${ROLLBACK_STAGE}" \
