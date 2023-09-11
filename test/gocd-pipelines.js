c888f8d116e98019936610f7a0aa2a08c183876e
Matt Gaunt-Seo <matt.gaunt@sentry.io>
2023-09-11 13:36:48 -0700

    Add some basic tests for gocd-pipelines

---
+++ test/gocd-pipelines.js
@@ -0,0 +1,9 @@
+import test from 'ava';
+import {assert_testdata, get_fixtures} from './utils/testdata.js';
+
+const files = await get_fixtures('gocd-pipelines');
+for (const f of files) {
+  test(`render ${f}`, async t => {
+    await assert_testdata(t, f);
+  });
+}
