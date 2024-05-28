1fa45dc7ec6353625a58cc4897c49cdabcea6c15
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2024-05-28 10:06:02 -0700

    Adding ability to dicatate pause message using env vars (#73)

--- libs/bash/pause-pipelines.sh
+++ libs/bash/pause-pipelines.sh
@@ -6,7 +6,10 @@ if [[ "${ALL_PIPELINE_FLAGS:-}" ]]; then
   set -- $ALL_PIPELINE_FLAGS
 fi
 
+# Set the pause message to the value of the PAUSE_MESSAGE environment variable or use the default message
+PAUSE_MESSAGE="${PAUSE_MESSAGE:-This pipeline is being rolled back, please check with team before un-pausing.}"
+
 # Pause all pipelines in the pipedream
 gocd-pause-and-cancel-pipelines \
-  --pause-message="This pipeline is being rolled back, please check with team before un-pausing." \
+  --pause-message="$PAUSE_MESSAGE" \
   "$@"
