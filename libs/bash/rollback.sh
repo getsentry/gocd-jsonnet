15bd12e318768e68b3f3516085d23ec8d66f2701
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2024-06-03 14:57:09 -0700

    Adding TRIGGERED_BY env var to enhance the pause message

--- libs/bash/rollback.sh
+++ libs/bash/rollback.sh
@@ -6,6 +6,16 @@ if [[ "${REGION_PIPELINE_FLAGS:-}" ]]; then
   set -- $REGION_PIPELINE_FLAGS
 fi
 
+# The user that triggered the rollback
+TRIGGERED_BY="${TRIGGERED_BY:-}"
+
+pause_message='This pipeline was rolled back, please check with team before un-pausing.'
+
+# Include triggered by in the pause message if it is not empty
+if [ -n "$TRIGGERED_BY" ]; then
+  pause_message="$pause_message Triggered by: $TRIGGERED_BY"
+fi
+
 # Get sha from the given pipeline run to deploy to all pipedream pipelines.
 sha=$(gocd-sha-for-pipeline --material-name="${ROLLBACK_MATERIAL_NAME}")
 
@@ -15,5 +25,5 @@ gocd-emergency-deploy \
   --material-name="${ROLLBACK_MATERIAL_NAME}" \
   --commit-sha="${sha}" \
   --deploy-stage="${ROLLBACK_STAGE}" \
-  --pause-message="This pipeline was rolled back, please check with team before un-pausing." \
+  --pause-message="$pause_message" \
   "$@"
