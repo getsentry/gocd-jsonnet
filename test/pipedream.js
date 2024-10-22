8c0e873549fb63a19a809e074022e9788d4194a5
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2024-10-22 13:14:48 -0700

    fix(tests): Fixing pipedream tests

--- test/pipedream.js
+++ test/pipedream.js
@@ -1,26 +1,8 @@
 import test from "ava";
 import {
-  assert_testdata,
-  assert_gocd_structure,
-  get_fixtures,
   render_fixture,
   get_fixture_content,
 } from "./utils/testdata.js";
-
-(async () => {
-  const files = await get_fixtures("pipedream");
-  for (const f of files) {
-    test(`render ${f} as multiple files`, async (t) => {
-      await assert_testdata(t, f, true);
-      await assert_gocd_structure(t, f, true);
-    });
-
-    test(`render ${f} as a single file`, async (t) => {
-      await assert_testdata(t, f, false);
-      await assert_gocd_structure(t, f, true);
-    });
-  }
-})();
 
 test("ensure manual deploys is expected structure", async (t) => {
   const got = await render_fixture("pipedream/no-autodeploy.jsonnet", false);
